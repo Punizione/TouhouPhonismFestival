38b2bfe08669c5311518b506e399a10328f4bf74
Punizione <1273435147@qq.com>
2018-07-20 22:38:52 +0800

    update

---
+++ api/utils/TestCommon.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+class TestCommon(Object):
+	def __init__(self):
+		pass
+
+
+	@static_method
+	def randomStart(ip, randomID, difficulty, length):
+
