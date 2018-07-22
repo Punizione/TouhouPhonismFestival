5ba689c2d183c5ef81e0b6a594f4c161be3a1752
Punizione <1273435147@qq.com>
2018-07-22 16:39:32 +0800

    update

---
+++ App/api/security.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+""" Security Related things """
+from functools import wraps
+from flask_restful import abort
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+from App import app
+import jwt
+import datetime
+import time
+from flask import request, g
+def require_auth(func):
+    """ Secure method decorator """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        header = request.headers.get("Authorization")
+        if not header:
+            abort(401, message='Not Token')
+        else:
+            _, token = header.split()
+            try:
+                decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms="HS256")
+            except jwt.DecodeError:
+                abort(400)
+            except jwt.ExpiredSignatureError:
+                abort(400)
+            if not decoded['token']:
+                abort(401)
+            else:
+                token = None #Todo  decoded['token']
+                if not token:
+                    abort(401)
+                else:
+                    g.token = token
+                    g.difficulty = decoded['difficulty']
+                    g.randomID = decoded['randomID']
+                    return func(*args, **kwargs)
+
