d8cc9c47ee5dce711c334fb4ead23afe52417cc4
Punizione <1273435147@qq.com>
2018-07-26 20:39:46 +0800

    update

--- App/api/rest/routing.py
+++ App/api/rest/routing.py
@@ -6,39 +6,47 @@ from flask import request,g
 import jwt
 from flask_restful import abort
 from App.api.rest.base import BaseResource, SecureResource, rest_resource
-from TestCommon import TestCommon
-import Constants
+from App.api.utils.TestCommon import TestCommon
+import App.api.utils.Constants as Constants
 import hashlib
+import datetime
+import time
+
+from App import app
 
 @rest_resource
 class Start(BaseResource):
 	""" /api/start """
 	endpoints = ['/start']
 
-	def get(self):
-		difficulty = request.args.get('difficulty')
-		randomID = request.args.get('random')
+	def post(self):
+		jsonPayload = request.json
+		difficulty = jsonPayload['difficulty']
+		randomID = jsonPayload['randomID']
+		typ = jsonPayload['typ']
 		#token = request.headers['X-Forwarded-For']
 		#token is ip
 		ip = request.remote_addr
-		length = request.args.get('length')
+		length = jsonPayload['length']
+		name = jsonPayload['name']
 		'''
 			TODO
 			从redis中获取ip信息映射,
 
 		''' 
 		exp = datetime.datetime.utcnow() + datetime.timedelta(hours=app.config['SECURITY_TOKEN_USER_HOUR'])
+		token = hashlib.md5((ip+randomID).encode('utf8')).hexdigest()
 		encode = jwt.encode({
-			'token': hashlib.md5(ip+randomID),
+			'token': token,
 			'difficulty': difficulty,
 			'exp': exp
 		}, app.config['SECRET_KEY'], algorithm="HS256")
 
-
+		#TestCommon.savePlayer(token, name)
 		first = TestCommon.randomStart(token, typ, difficulty, length)
 		return {
 			'retCode': Constants.SUCCESS,
-			'token': encode,
+			'token': encode.decode('utf-8'),
 			'question': first
 		}
 
@@ -48,19 +56,18 @@ class Submit(SecureResource):
 	""" /api/next """
 	endpoints = ['/next']
 
-	def get(self):
-		answer = request.args.answer
-		if answer == Constants.COMPLETE:
-			_next = TestCommon.getResult(g)
+	def post(self):
+		jsonPayload = request.json
+		answer = jsonPayload['answer']
+		_next = TestCommon.nextQuestion(answer, g)
+		flag = Constants.HAVE_NEXT
+		if _next ==  Constants.COMPLETE:
 			flag = Constants.END
-		else:
-			_next = TestCommon.nextQuestion(answer, g)
-			flag = Constants.HAVE_NEXT
-			if _next ==  Constants.COMPLETE:
-				flag = Constants.END
+			_next = TestCommon.getResult(g)
 		return {
 			'retCode': Constants.SUCCESS,
 			'haveNext': flag,
 			'next': _next
 		}
+
 
