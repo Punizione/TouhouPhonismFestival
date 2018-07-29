9636046a443beefc2ef9c4a9f81034bf17eaa02c
Punizione <1273435147@qq.com>
2018-07-29 18:25:33 +0800

    update

--- App/api/utils/FileCommon.py
+++ App/api/utils/FileCommon.py
@@ -5,13 +5,16 @@ import os
 from pydub import AudioSegment
 import uuid as UUID
 import base64
-import Constants
-class FileCommon(Object):
+import App.api.utils.Constants as Constants
+import random
+from App.api.utils.SplitTooLongError import SplitTooLongError
+
+class FileCommon():
 	def __init__(self):
 		super().__init__()
 
-	@class_method
-	def combine(typ):
+	@classmethod
+	def combine(cls, typ):
 		infoDict = {}
 		if type(typ) == list:
 			for i in typ:
@@ -20,21 +23,23 @@ class FileCommon(Object):
 					infoDict.update(json.load(load))
 		return infoDict.keys()
 
-	@class_method
-	def gen(item, difficulty):
+	@classmethod
+	def gen(cls, item, difficulty):
+		if type(item) == bytes:
+			item = item.decode('utf8')
 		path = Constants.MUSIC_PATH_PREFIX + item + Constants.MUSIC_PATH_SUFFIX
-		song = AudioSegment.from_mp3(item)
+		song = AudioSegment.from_mp3(path)
 		seconds = Constants.DIFFICULTY_LEVEL[difficulty] * Constants.SECOND
 
 		length = len(song)
 		if length - seconds - (2*Constants.BEGIN) < 0:
-			raise SplitTooLongException(item)
+			raise SplitTooLongError(item)
 
 		begin = random.randint(Constants.BEGIN, length-seconds-Constants.BEGIN)
 		fragment = song[begin:begin+seconds]
 		if difficulty == Constants.DIFFICULTY_EXTRA:
 			fragment = fragment.reverse()
-		name = base64.urlsafe_b64encode(UUID.uuid4().bytes)[:-2].encode('utf-8')
+		name = base64.urlsafe_b64encode(UUID.uuid4().bytes)[:-2].decode('utf-8')
 		fragment.export(Constants.MUSIC_FRAGMENT_PATH+name+Constants.MUSIC_FILE_FORMAT, format="mp3")
 		return name
 
