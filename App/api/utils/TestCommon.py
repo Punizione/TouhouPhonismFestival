46ac65ea9101a8868cc289e500e07f48001ed734
Punizione <1273435147@qq.com>
2018-08-01 20:31:49 +0800

    update

---
+++ App/api/utils/TestCommon.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import random
+from App.api.utils.FileCommon import FileCommon
+import App.api.utils.RedisCommon as RedisCommon
+import App.api.utils.Constants as Constants
+class TestCommon():
+	def __init__(self):
+		super().__init__()
+
+
+	@classmethod
+	def randomStart(cls, token, typ, difficulty, length):
+		li = cls.genQuestionList(typ, length)
+		#TODO
+		"""
+		保存li内容到redis
+		"""
+		RedisCommon.saveQuestion(token, li)
+		return FileCommon.gen(li[0], difficulty)
+
+	@classmethod
+	def nextQuestion(cls, answer, g):
+		token = g.token
+		difficulty = g.difficulty
+		#TODO
+		"""
+		从redis中取出余下问题链接和答案结果链接
+		"""
+		questionList = RedisCommon.getQuestion(token)
+		correct = RedisCommon.getCorrect(token)
+		checkList = RedisCommon.getCheck(token)
+
+		result = cls.check(questionList[len(checkList)], answer)
+		if result:
+			correct += 1
+			#TODO
+			"""
+				保存分数，答对题数
+			"""
+			RedisCommon.saveCorrect(token, correct)
+
+		#TODO
+		"""
+			答题内容
+		"""
+		RedisCommon.saveCheck(token, answer)
+		print('%d-%d' %(len(checkList), len(questionList)))
+		if len(checkList) == len(questionList)-1:
+			return Constants.COMPLETE
+		return FileCommon.gen(questionList[len(checkList)+1], difficulty)
+
+
+	@classmethod
+	def getResult(cls, g):
+		token = g.token
+		difficulty = g.difficulty
+		#TODO
+		"""
+			从redis中取出所有问题链接和答案结果链接与评分
+		"""
+		questionList = RedisCommon.getQuestion(token)
+		correct = RedisCommon.getCorrect(token)
+		checkList = RedisCommon.getCheck(token)
+
+		retList = []
+		for i in range(len(questionList)):
+			retList.append({
+				"q": questionList[i].decode('utf8'),
+				"a": checkList[i].decode('utf8')
+			})
+		# ,"correct": correct
+		return {
+			"data": retList
+		}
+
+	@classmethod
+	def check(cls, question, answer):
+		return question == answer
+
+
+	@classmethod
+	def genQuestionList(cls, typ, length):
+		return random.sample(FileCommon.combine(typ), length)
+
+
+	@classmethod
+	def savePlayer(cls,token, name):
+		RedisCommon.savePlayer(token, name)
+
+
+
