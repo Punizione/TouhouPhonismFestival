c78832ddcd025d0a2b2b4d28c3ae2dc146dd3b7b
Punizione <1273435147@qq.com>
2018-08-24 13:56:29 +0800

    remove useless info

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__, static_url_path='')
@@ -11,11 +10,7 @@ app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = "Delitto"
 app.config['SECURITY_TOKEN_AUTHENTICATION_HEADER'] = "Delitto"
 
-
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:000000@127.0.0.1:3306/vma'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from . import config
 app.logger.info('>>> {}'.format(app.config['MODE']))
