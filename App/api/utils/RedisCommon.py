9f16a3c10c130caffc5c54930155725b3cd2f33e
Punizione <1273435147@qq.com>
2018-09-07 08:27:18 +0800

    update

--- App/api/utils/RedisCommon.py
+++ App/api/utils/RedisCommon.py
@@ -14,14 +14,22 @@ questionPool = redis.ConnectionPool(
 	port=Constants.REDIS_PORT,
 	db=Constants.REDIS_DB_QUESTION
 	)
-checkPool =redis.ConnectionPool(
+checkPool = redis.ConnectionPool(
 	host=Constants.REDIS_HOST,
 	port=Constants.REDIS_PORT,
 	db=Constants.REDIS_DB_CHECK
 	)
+
+rankPool = redis.ConnectionPool(
+	host=Constants.REDIS_HOST,
+	port=Constants.REDIS_PORT,
+	db=Constants.REDIS_DB_RANK
+	)
+
 instance = redis.Redis(connection_pool=pool)
 questionInstance = redis.Redis(connection_pool=questionPool)
 checkInstance = redis.Redis(connection_pool=checkPool)
+rankInstance = redis.Redis(connection_pool=rankPool)
 def saveQuestion(token, li):
 	for item in li:
 		questionInstance.rpush(token, item)
@@ -49,6 +57,30 @@ def saveCheck(token, result):
 def savePlayer(token, name):
 	instance.hset(Constants.PLAYER, token, name)
 
-def saveRanking(token):
-	pass
+def getPlayer(token):
+	player = instance.hget(Constants.PLAYER, token)
+	return player
+
+def saveRanking(token, difficulty):
+	correct = getCorrect(token)
+	player = getPlayer(token)
+	# reverse command with score and member in Redis-Py
+	# zadd `key` `score` `member`
+	rankInstance.zadd(difficulty, player, correct)
+	'''
+	ranking always save top 10
+	so when put 1 member into, remove 1 member
+	'''
+	rankInstance.zremrangebyrank(difficulty, 0, 1)
+
+def getRanking(difficulty):
+	rank = rankInstance.zrange(difficulty, 0, 10)
+	result = []
+	for i in rank:
+		result.append({
+			"name": i,
+			"grade": getCorrect(i)
+			})
+	return result
+
 
